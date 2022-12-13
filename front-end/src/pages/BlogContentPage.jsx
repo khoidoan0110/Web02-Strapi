dd8b132eb22480f19e81019ea8d36a82f041d12f
Khôi <63962075+khoidoan0110@users.noreply.github.com>
2022-12-13 16:33:08 +0700

    Add files via upload

---
+++ front-end/src/pages/BlogContentPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar, BlogContent, Footer } from "../components";
+
+const BlogContentPage = ({blogs}) => {
+  return (
+    <div>
+      <Navbar />
+      <BlogContent blogs={blogs}/>
+      <Footer />
+    </div>
+  );
+};
+
+export default BlogContentPage;
